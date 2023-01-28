2fdce96b565c2636c1b71dd0bba7391bb7413a0c
miacias <miaciasullo@gmail.com>
2023-01-28 10:57:13 -0500

    SOLVED: license link is now hyperlinked to the badge instead of in text

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,54 +8,6 @@ function renderLicenseBadge(licenseQ) {
   } else {
     return "";
   }
-  // switch (licenseQ) {
-  //   case "Academic Free License v3.0" :
-  //     licenseCode.push("");
-  //     break;
-  //   case "Apache license 2.0":
-  //     licenseCode.push("Apache2.0");
-  //     break;
-  //   case "Artistic license 2.0":
-  //     licenseCode.push("");
-  //     break;
-  //   case "Boost Software License 1.0":
-  //     licenseCode.push("");
-  //     break;
-  //   case "BSD 2-clause \"Simplified\" license":
-  //     licenseCode.push("");
-  //     break;
-  //   case "BSD 3-clause \"New\" or \"Revised\" license":
-  //     licenseCode.push("");
-  //     break;
-  //   case "BSD 3-clause Clear license":
-  //     licenseCode.push("");
-  //     break;
-  // }
-  return licenseCode[0];
-  // "Creative Commons Zero v1.0 Universal",
-  // "Creative Commons Attribution 4.0",
-  // "Creative Commons Attribution Share Alike 4.0",
-  // "Do What The F*ck You Want To Public License",
-  // "Educational Community License v2.0",
-  // "Eclipse Public License 1.0",
-  // "Eclipse Public License 2.0",
-  // "European Union Public License 1.1",
-  // "GNU General Public License v2.0",
-  // "GNU General Public License v3.0",
-  // "GNU Lesser General Public License v2.1",
-  // "GNU Lesser General Public License v3.0",
-  // "ISC",
-  // "LaTeX Project Public License v1.3c",
-  // "Microsoft Public License",
-  // MIT: "MIT",
-  // "Mozilla Public License 2.0",
-  // "Open Software License 3.0",
-  // "PostgreSQL License",
-  // "SIL Open Font License 1.1",
-  // "University of Illinois/NCSA Open Source License",
-  // "The Unlicense",
-  // "zLib License",
-  // }
 }
 
 // returns a license link or an empty string
@@ -68,12 +20,11 @@ function renderLicenseLink(licenseQ, usernameQ, titleQ) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// returns the license section of README or an empty string
 function renderLicenseSection(licenseQ, usernameQ, titleQ) {
 return `
 ${licenseQ}
-${renderLicenseBadge(licenseQ)}${renderLicenseLink(licenseQ, usernameQ, titleQ)}`
+[${renderLicenseBadge(licenseQ)}]${renderLicenseLink(licenseQ, usernameQ, titleQ)}`
 }
 
 // gets each step separately
